37d7b673d83a3a2024258b8199384d0e82656182
limxing <474631895@qq.com>
2017-05-11 16:58:41 +0800

    添加进入时能否非动画

--- promptlibrary/src/main/java/me/leefeng/promptlibrary/PromptDialog.java
+++ promptlibrary/src/main/java/me/leefeng/promptlibrary/PromptDialog.java
@@ -30,9 +30,14 @@ public class PromptDialog {
     private ValueAnimator dissmissAnim;
     private boolean dissmissAnimCancle;
     private boolean outAnimRunning;
-    private long viewAnimDuration = 200;
+    private long viewAnimDuration = 300;
     private boolean isShowing;
 
+    /**
+     * 设置进入 进出动画持续的事件默认300毫秒
+     *
+     * @param viewAnimDuration 毫秒
+     */
     public void setViewAnimDuration(long viewAnimDuration) {
         this.viewAnimDuration = viewAnimDuration;
     }
@@ -92,7 +97,7 @@ public class PromptDialog {
      * 立刻关闭窗口
      */
     public void dismissImmediately() {
-        if (isShowing) {
+        if (isShowing && !outAnimRunning) {
             decorView.removeView(promptView);
             isShowing = false;
         }
@@ -104,7 +109,7 @@ public class PromptDialog {
     public void dismiss() {
 
         if (isShowing && !outAnimRunning) {
-            if (promptView.getBuilder().withAnim) {
+            if (promptView.getBuilder().withAnim && outAnim != null) {
 //                outAnim.setStartOffset(delayTime);
                 if (promptView.getCurrentType() == PromptView.PROMPT_LOADING) {
                     outAnim.setStartOffset(promptView.getBuilder().loadingDuration);
@@ -139,21 +144,37 @@ public class PromptDialog {
     }
 
     public void showError(String msg) {
-        showSomthing(R.drawable.ic_prompt_error, PromptView.PROMPT_ERROR, msg);
+        showError(msg, true);
+
+    }
+
+    public void showError(String msg, boolean withAnim) {
+        showSomthing(R.drawable.ic_prompt_error, PromptView.PROMPT_ERROR, msg, withAnim);
 
     }
 
     public void showInfo(String msg) {
-        showSomthing(R.drawable.ic_prompt_info, PromptView.PROMPT_INFO, msg);
+        showInfo(msg, true);
+    }
+
+    public void showInfo(String msg, boolean withAnim) {
+        showSomthing(R.drawable.ic_prompt_info, PromptView.PROMPT_INFO, msg, withAnim);
     }
 
     public void showWarn(String msg) {
-        showSomthing(R.drawable.ic_prompt_warn, PromptView.PROMPT_WARN, msg);
+        showWarn(msg, true);
+    }
+
+    public void showWarn(String msg, boolean withAnim) {
+        showSomthing(R.drawable.ic_prompt_warn, PromptView.PROMPT_WARN, msg, withAnim);
     }
 
     public void showSuccess(String msg) {
+        showSuccess(msg, true);
+    }
 
-        showSomthing(R.drawable.ic_prompt_success, PromptView.PROMPT_SUCCESS, msg);
+    public void showSuccess(String msg, boolean withAnim) {
+        showSomthing(R.drawable.ic_prompt_success, PromptView.PROMPT_SUCCESS, msg, withAnim);
     }
 
     /**
@@ -163,19 +184,19 @@ public class PromptDialog {
      * @param msg
      */
     public void showCustom(int icon, String msg) {
-//        Builder builder = Builder.getDefaultBuilder();
-//        builder.icon(icon);
-//        builder.text(msg);
-
-        showSomthing(icon, PromptView.PROMPT_CUSTOM, msg);
+        showCustom(icon, msg, true);
     }
 
-    private void showSomthing(int icon, int promptError, String msg) {
+    public void showCustom(int icon, String msg, boolean withAnim) {
+        showSomthing(icon, PromptView.PROMPT_CUSTOM, msg, withAnim);
+    }
+
+    private void showSomthing(int icon, int promptError, String msg, boolean withAnim) {
         Builder builder = Builder.getDefaultBuilder();
         builder.text(msg);
         builder.icon(icon);
         closeInput();
-        checkLoadView();
+        checkLoadView(withAnim);
         if (isShowing) {
             promptView.setBuilder(builder);
             promptView.showSomthing(promptError);
@@ -184,45 +205,73 @@ public class PromptDialog {
     }
 
     public void showWarnAlert(String text, PromptButton button) {
-        showAlert(text, button);
+        showWarnAlert(text, button, false);
+    }
+
+    public void showWarnAlert(String text, PromptButton button, boolean withAnim) {
+        showAlert(text, withAnim, button);
+    }
+
+    /**
+     * 展示警告对话框
+     *
+     * @param text
+     * @param button1
+     * @param button2
+     * @param withAnim 是否动画进入
+     */
+    public void showWarnAlert(String text, PromptButton button1, PromptButton button2, boolean withAnim) {
+        showAlert(text, withAnim, button1, button2);
     }
 
     public void showWarnAlert(String text, PromptButton button1, PromptButton button2) {
-        showAlert(text, button1, button2);
+        showWarnAlert(text, button1, button2, true);
     }
 
-    private void showAlert(String text, PromptButton... button) {
+    private void showAlert(String text, boolean withAnim, PromptButton... button) {
         Builder builder = Builder.getAlertDefaultBuilder();
         builder.text(text);
         builder.icon(R.drawable.ic_prompt_alert_warn);
         closeInput();
-        checkLoadView();
+        checkLoadView(withAnim);
         promptView.setBuilder(builder);
         promptView.showSomthingAlert(button);
         dissmissAni(true);
 
     }
 
-    public void showLoading(String msg) {
+    /**
+     * 展示loading
+     *
+     * @param msg      信息
+     * @param withAnim 是否动画进入
+     */
+    public void showLoading(String msg, boolean withAnim) {
         Builder builder = Builder.getDefaultBuilder();
         builder.icon(R.drawable.ic_prompt_loading);
         builder.text(msg);
         promptView.setBuilder(builder);
         closeInput();
-        checkLoadView();
+        checkLoadView(withAnim);
         promptView.showLoading();
         dissmissAni(true);
+    }
+
+    public void showLoading(String msg) {
+        showLoading(msg, true);
 
     }
 
     /**
      * promptview isshowing
+     *
+     * @param withAnim
      */
-    private void checkLoadView() {
+    private void checkLoadView(boolean withAnim) {
         if (!isShowing) {
             decorView.addView(promptView);
             isShowing = true;
-            if (promptView.getBuilder().withAnim)
+            if (promptView.getBuilder().withAnim && inAnim != null && withAnim)
                 promptView.startAnimation(inAnim);
         }
     }
@@ -276,14 +325,29 @@ public class PromptDialog {
 //        window.closeAllPanels();
     }
 
+    /**
+     * 获取提示框的配置类
+     *
+     * @return
+     */
     public Builder getDefaultBuilder() {
         return Builder.getDefaultBuilder();
     }
 
+    /**
+     * 获取对话框的配置类
+     *
+     * @return
+     */
     public Builder getAlertDefaultBuilder() {
         return Builder.getAlertDefaultBuilder();
     }
 
+    /**
+     * 处理返回键，需要用户自行调用
+     *
+     * @return
+     */
     public boolean onBackPressed() {
         if (isShowing && promptView.getCurrentType() == PromptView.PROMPT_LOADING) {
             return false;
